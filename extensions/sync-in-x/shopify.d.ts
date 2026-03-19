6cf5650a01bd1ef9a46399c412e5df60701da160
bradfromaward <brad@awardrv.com.au>
2026-03-19 12:06:20 +1100

    Multi Fix

--- extensions/sync-in-x/shopify.d.ts
+++ extensions/sync-in-x/shopify.d.ts
@@ -2,6 +2,12 @@ import '@shopify/ui-extensions';
 
 //@ts-ignore
 declare module './src/ActionExtension.jsx' {
+  const shopify: import('@shopify/ui-extensions/admin.product-index.selection-action.render').Api;
+  const globalThis: { shopify: typeof shopify };
+}
+
+//@ts-ignore
+declare module './src/ActionExtension.jsx' {
   const shopify: import('@shopify/ui-extensions/admin.product-details.action.render').Api;
   const globalThis: { shopify: typeof shopify };
 }
